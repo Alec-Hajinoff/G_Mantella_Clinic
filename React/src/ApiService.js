76f661491910b3a0fec393081a651f2257ec6b8c
Alec Hajinoff <hajinoff@gmail.com>
2026-09-05 15:59:29 +0100

    5/9/26, 15:59

--- React/src/ApiService.js
+++ React/src/ApiService.js
@@ -415,7 +415,7 @@ export const customerDeleteAccount = async () => {
   }
 };
 
-// adminBookingsList() fetchs all garage bookings for Owner/Admin/Mechanic roles
+// adminBookingsList() fetchs all garage bookings for Owner/Admin roles
 
 export const adminBookingsList = async () => {
   try {
